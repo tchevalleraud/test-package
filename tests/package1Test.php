80ec77e22298e2d617c5fa292352278a784f0382
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2022-02-13 23:44:18 +0100

    update

--- tests/package1Test.php
+++ tests/package1Test.php
@@ -6,4 +6,9 @@
             $this->assertInstanceOf(\Tchevalleraud\TestPackage\package1::class, $package);
         }
 
+        public function testPackage1_construct(){
+            $package = new \Tchevalleraud\TestPackage\package1();
+            $this->assertEquals(new \Tchevalleraud\TestPackage\package1(), $package);
+        }
+
     }
